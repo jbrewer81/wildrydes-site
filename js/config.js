e9c8a0ecd6e0aab96d85c1a52c7f47327d46e167
Jermaine Brewer <chowrunner2000@gmail.com>
2024-12-22 17:06:30 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_t2vmy55OD', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_AwbfCOKC4', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '798c9flqu5u7qrcb7f9lndd05n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
